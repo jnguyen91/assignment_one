d42e2bf1e12b9ec48b18f7117046f5dedc77a075
Lenic Zhang <lenic.zhang@gmail.com>
2017-10-08 12:13:54 -0700

    Update menu bar, add fleet page, add airplanes model, update autoload file

---
+++ application/models/Airplanes.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This is a "CMS" model for quotes, but with bogus hard-coded data,
+ * so that we don't have to worry about any database setup.
+ * This would be considered a "mock database" model.
+ *
+ */
+class Airplanes extends CI_Model
+{
+	var $data = array(
+		'1'	 => array('vehicleID' => '001', 'id' => 'citation', 'manufacturer'	=> 'Cessna', 
+					'model' => 'Citation M2', 'price' => '3200', 'seats' => '7',
+					'reach' => '1550', 'cruise' => '748', 'takeoff' => '978', 
+					'hourly' => '1122'),
+		'2'	 => array('vehicleID' => '002', 'id' => 'mustang', 'manufacturer' => 'Cessna', 
+					'model'	=> 'Citation Mustang', 'price'	=> '2770', 'seats' => '4',
+					'reach' => '2130', 'cruise' => '630', 'takeoff' => '950', 
+					'hourly' => '1015'),
+		'3'	 => array('vehicleID' => '003', 'id' => 'kingair', 'manufacturer' => 'Beechcraft', 
+					'model'	=> 'King Air C90', 'price' => '3900', 'seats' => '12',
+					'reach' => '2446', 'cruise' => '500', 'takeoff' => '1402', 
+					'hourly' => '990')		
+	);
+
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();
+
+		// inject each "record" key into the record itself, for ease of presentation
+		foreach ($this->data as $key => $record)
+		{
+			$record['key'] = $key;
+			$this->data[$key] = $record;
+		}
+	}
+
+	// retrieve a single airplane, null if not found
+	public function get($which)
+	{
+		return !isset($this->data[$which]) ? null : $this->data[$which];
+	}
+
+	// retrieve all of the airplanes
+	public function all()
+	{
+		return $this->data;
+	}
+}
---
+++ application/views/welcome.php
@@ -0,0 +1,20 @@
+<h1>Welcome to CodeIgniter!</h1>
+
+<div id="body">
+	<div>
+		<h4>YellowHammer</h4>
+		<ul>
+			<li type="circle">Number of planes: 3</li>
+			<li type="circle">Number of flights everyday: TBD</li>
+		</ul>
+	</div>
+	<div>
+		<h4>Base: YZY</h4>
+		<ul>
+			<li type="circle">Base: YZY</li>
+			<li type="circle">Destination 1: YQZ</li>
+			<li type="circle">Destination 2: ZEL</li>
+			<li type="circle">Destination 3: YGB</li>
+		</ul>
+	</div>
+</div>
