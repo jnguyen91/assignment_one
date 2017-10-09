c633be8f54e543922fa2d9386bab8e9bd20672bf
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-10-08 20:51:50 -0700

    welcome php controllers

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,11 +1,21 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+/**
+ * Welcome controller.
+ *
+ * @author Jeffrey Nguyen
+ */
 class Welcome extends Application
 {
-
+	
 	/**
+	 * Constructor.
+	 */
+	function __construct()
+    {
+        parent::__construct();
+    }
+    /**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
@@ -17,11 +27,31 @@ class Welcome extends Application
 	 * map to /welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+    public function index()
 	{
-		$this->data['pagebody'] = 'homepage';
-                
-		$this->render(); 
+	    $airplanes = $this->airplanes->all();
+	    $flights = $this->flight->all();
+	    $numSchedules = 0;
+	    $airports = $this->airports->all();
+	    $base = 'No Base Airport';
+	    $destinations = array();
+	    foreach ($flights as $flights) {
+	    	if (strcmp($flights['day'], 'Monday') == 0) {
+                $numSchedules++;
+            }
+	    }
+	    foreach ($airports as $airports) {
+	    	if (strcmp($airports['type'], 'base') == 0) {
+                $base = $airports['airport'];
+            } else {
+            	array_push($destinations, $airports);
+            }
+	    }
+		$this->data['pagebody'] = 'welcome_message';
+		$this->data['numAirplanes'] = count($airplanes);
+	    $this->data['numFlights'] = $numSchedules;
+        $this->data['base'] = $base;
+        $this->data['destinations'] = $destinations;
+		$this->render();
 	}
-
 }
