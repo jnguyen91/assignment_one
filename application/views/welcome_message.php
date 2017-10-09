f0ce5baa32a3cd6d9010913cf92d7f83cc679943
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-10-08 18:37:19 -0700

    welcome message

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<h2>Welcome!</h2>
+<div>
+	<div>
+		<p><b>Number of Planes</b></p>
+		<ol>{numAirplanes}</ol>
+	</div>
+	<div>
+		<p><b>Number of Flights Scheduled</b></p>
+		<ol>{numFlights}</ol>
+	</div>
+	<div>
+		<p><b>Base Airport</b></p>
+		<ol>{base}</ol>
+	</div>
+	<div>
+		<b>Destinations</b>
+		{destinations}
+			<ul>{airport}</ul>
+		{/destinations}
+	</div>
+</div>
