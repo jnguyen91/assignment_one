19eed8c6cc9658f85950c9bb4a8f2814995e52b6
Ian Park <bcitipark15@gmail.com>
2017-11-13 01:58:54 -0800

    Excluded the WackyData model from being auto-loaded

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,4 +133,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('airplane', 'airplanes', 'airport', 'airports', 'flight', 'flights', 'wackydata');
+$autoload['model'] = array('airplane', 'airplanes', 'airport', 'airports', 'flight', 'flights');
